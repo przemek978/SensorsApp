0fc96455b516f0d5c0651440e7707622f71f7a69
przemek978 <przemek.ku2000@gmail.com>
2022-11-20 23:35:46 +0100

    working

--- app/src/main/java/com/example/sensorapp/LocationActivity.java
+++ app/src/main/java/com/example/sensorapp/LocationActivity.java
@@ -1,17 +1,18 @@
 package com.example.sensorapp;
 
-import static androidx.constraintlayout.motion.widget.Debug.getLocation;
-
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,15 @@ import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 
 public class LocationActivity extends AppCompatActivity {
@@ -40,9 +50,9 @@ public class LocationActivity extends AppCompatActivity {
         getLocationButton.setOnClickListener((View v)->getLocation());
         locationTextView = findViewById(R.id.textview_location);
        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        //addressTextView = findViewById(R.id.textview_address);
-        //Button getAddressButton = findViewById(R.id.get_address_button);
-        //getAddressButton.setOnClickListener(v->executeGeocoding());
+        addressTextView = findViewById(R.id.textview_address);
+        Button getAddressButton = findViewById(R.id.get_address_button);
+        getAddressButton.setOnClickListener(v->executeGeocoding());
     }
     private void getLocation() {
         if (ContextCompat.checkSelfPermission(this,
@@ -75,6 +85,51 @@ public class LocationActivity extends AppCompatActivity {
                     Toast.makeText(this, R.string.location_permission_denied, Toast.LENGTH_SHORT).show();
                 }
                 break;
+        }
+    }
+    private String locationGecoding(Context context, Location location){
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        List<Address> addresses = null;
+        String resultMessage = "";
+
+        try {
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        }
+        catch (IOException ioException){
+            resultMessage = context.getString(R.string.service_not_available);
+            Log.e(TAG, resultMessage, ioException);
+        }
+
+        if(addresses == null || addresses.isEmpty()){
+            if(resultMessage.isEmpty()){
+                resultMessage = context.getString(R.string.no_address_found);
+                Log.e(TAG, resultMessage);
+            }
+        }
+        else{
+            Address address = addresses.get(0);
+            List<String> addressParts = new ArrayList<>();
+
+            for(int i=0; i<=address.getMaxAddressLineIndex(); i++){
+                addressParts.add(address.getAddressLine(i));
+            }
+            resultMessage = TextUtils.join("\n", addressParts);
+        }
+        return resultMessage;
+    }
+
+    private void executeGeocoding(){
+        if(lastLocation != null){
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            Future<String> returnedAddress = executor.submit(()->locationGecoding(getApplicationContext(),lastLocation));
+            try{
+                String result = returnedAddress.get();
+                addressTextView.setText(getString(R.string.address_text, result, System.currentTimeMillis()));
+            }
+            catch(ExecutionException | InterruptedException e){
+                Log.e(TAG, e.getMessage(), e);
+                Thread.currentThread().interrupt();
+            }
         }
     }
 }
--- app/src/main/java/com/example/sensorapp/SensorDetailsActivity.java
+++ app/src/main/java/com/example/sensorapp/SensorDetailsActivity.java
@@ -17,6 +17,7 @@ public class SensorDetailsActivity extends AppCompatActivity implements SensorEv
     private SensorManager sensorManager;
     private Sensor sensorLight,sensorAccel;
     //private TextView sensorLightTextView;
+    private Sensor sensor;
     private TextView sensorNameTextView;
     private TextView sensorValueTextView;
 
@@ -28,18 +29,17 @@ public class SensorDetailsActivity extends AppCompatActivity implements SensorEv
         sensorNameTextView = findViewById(R.id.details_sensor_name);
         sensorValueTextView = findViewById(R.id.details_sensor_value);
 //
-//        int sensorType = getIntent().getIntExtra(EXTRA_SENSOR_TYPE_PARAMETER, Sensor.TYPE_LIGHT);
+
         //sensorLightTextView=findViewById(R.id.sensor_light_label);
-
+        int sensorType = getIntent().getIntExtra(EXTRA_SENSOR_TYPE_PARAMETER, Sensor.TYPE_LIGHT);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensorLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensor = sensorManager.getDefaultSensor(sensorType);
 
-        if (sensorLight == null) {
+        if (sensor== null) {
             sensorNameTextView.setText(R.string.missing_sensor);
         }
-        if (sensorAccel == null) {
-            sensorNameTextView.setText(R.string.missing_sensor);
+        else{
+            sensorNameTextView.setText(sensor.getName());
         }
 
     }
@@ -47,9 +47,8 @@ public class SensorDetailsActivity extends AppCompatActivity implements SensorEv
     @Override
     protected void onStart() {
         super.onStart();
-        if (sensorLight != null) {
-            sensorManager.registerListener(this, sensorLight, SensorManager.SENSOR_DELAY_NORMAL);
-            sensorManager.registerListener(this, sensorAccel, SensorManager.SENSOR_DELAY_NORMAL);
+        if (sensor!= null) {
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
 
@@ -65,11 +64,13 @@ public class SensorDetailsActivity extends AppCompatActivity implements SensorEv
         float currentValue = sensorEvent.values[0];
         switch (sensorType) {
             case Sensor.TYPE_LIGHT:
-                sensorNameTextView.setText(getResources().getString(R.string.light_sensor_label,currentValue));
+                sensorValueTextView.setText(getResources().getString(R.string.sensor_label,currentValue));
                 break;
             case Sensor.TYPE_ACCELEROMETER:
-                sensorNameTextView.setText(getResources().getString(R.string.light_sensor_label,currentValue));
+                sensorValueTextView.setText(getResources().getString(R.string.sensor_label2, sensorEvent.values[0], sensorEvent.values[1], sensorEvent.values[2]));
                 break;
+
+
         }
 
     }
